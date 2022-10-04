d6b12482bb83fbdeff9ae6b9cea718115e0573d0
mmatveyev <progl@progl.su>
2022-10-04 16:29:29 +0300

    bash script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 mkdir -p /var/web-hpg/
 
 git clone  https://github.com/WEGA-project/web-hpg.git /var/web-hpg
-cd /var/web-hpg/web-hpg/
+cd /var/web-hpg/wega-hpg/
 MYSQL=$(which mysql)
 DB_PASS=$(echo "<?php include '/var/WEGA/db.php'; echo \$password;" | /usr/bin/php)
 echo "Create wega-hpg db name: $MAIN_DB_NAME"
@@ -20,20 +20,20 @@ echo "Install python"
 a2enmod proxy_http
 apt-get install --yes --quiet --no-install-recommends python3-pip python3-venv build-essential libssl-dev libffi-dev python3-dev libmysqlclient-dev libev-dev python3-wheel
 deactivate
-rm -R /var/web-hpg/web-hpg/venv
-python3 -m venv /var/web-hpg/web-hpg/venv
-source /var/web-hpg/web-hpg/venv/bin/activate
+rm -R /var/web-hpg/wega-hpg/venv
+python3 -m venv /var/web-hpg/wega-hpg/venv
+source /var/web-hpg/wega-hpg/venv/bin/activate
 pip3 install wheel
-pip3 install -r /var/web-hpg/web-hpg/requirements.txt
-chmod 744 /var/web-hpg/web-hpg/entrypoint.sh
-cp -f /var/web-hpg/web-hpg/scripts/wega-hpg.service /etc/systemd/system/wega-hpg.service
+pip3 install -r /var/web-hpg/wega-hpg/requirements.txt
+chmod 744 /var/web-hpg/wega-hpg/entrypoint.sh
+cp -f /var/web-hpg/wega-hpg/scripts/wega-hpg.service /etc/systemd/system/wega-hpg.service
 chmod 664 /etc/systemd/system/wega-hpg.service
 ln -s /var/WEGA/apache/wega-hpg.conf /etc/apache2/conf-enabled/
 mkdir -p /var/log/gunicron/
 a2enmod headers
 python manage_prod.py collectstatic --noinput
-touch /var/web-hpg/web-hpg/WEGA_HPG_PASSWORD
-WEGA_HPG_PASSWORD=$(cat /var/web-hpg/web-hpg/WEGA_HPG_PASSWORD)
+touch /var/web-hpg/wega-hpg/WEGA_HPG_PASSWORD
+WEGA_HPG_PASSWORD=$(cat /var/web-hpg/wega-hpg/WEGA_HPG_PASSWORD)
 if [[ -n "$WEGA_HPG_PASSWORD" ]]; then
   echo "we already have it"
 else
@@ -42,9 +42,9 @@ else
 fi
 # P_STRING="from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@localhost.ru', '$WEGA_HPG_PASSWORD')"
 # echo $P_STRING | python manage_prod.py shell
-echo $WEGA_HPG_PASSWORD >/var/web-hpg/web-hpg/WEGA_HPG_PASSWORD
-echo "WEGA_DEFAULT_USER = 'admin@localhost.ru'" > /var/web-hpg/web-hpg/project/default_user.py
-echo "WEGA_DEFAULT_PASSWORD = '$WEGA_HPG_PASSWORD'" >> /var/web-hpg/web-hpg/project/default_user.py
+echo $WEGA_HPG_PASSWORD >/var/web-hpg/wega-hpg/WEGA_HPG_PASSWORD
+echo "WEGA_DEFAULT_USER = 'admin@localhost.ru'" > /var/web-hpg/wega-hpg/project/default_user.py
+echo "WEGA_DEFAULT_PASSWORD = '$WEGA_HPG_PASSWORD'" >> /var/web-hpg/wega-hpg/project/default_user.py
 python manage_prod.py migrate --noinput
 
 systemctl daemon-reload
