09e3c24fb346351a06e07e71cbfd09412990ca0f
Mikhail M <neon2007@inbox.ru>
2023-01-22 20:52:55 +0300

    Update install_for_wega.sh

--- install_for_wega.sh
+++ install_for_wega.sh
@@ -20,7 +20,8 @@ $MYSQL -uroot -p$DB_PASS -e "$SQL"
 echo "Install python"
 a2enmod proxy_http
 apt update -y
-apt-get install --yes --quiet --no-install-recommends python3-pip python3-venv build-essential libssl-dev libffi-dev python3-dev libmysqlclient-dev libev-dev python3-wheel libxml2-dev libxslt-dev
+apt-get install --yes --quiet --no-install-recommends python3-pip python3-venv build-essential libssl-dev libffi-dev python3-dev libmysqlclient-dev libev-dev python3-wheel libxml2-dev libxslt-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev ython3-dev python3-setuptools
+    
 deactivate
 rm -R /var/web-hpg/app/venv
 python3 -m venv /var/web-hpg/app/venv
